4d4f216b54676602abd3b18e8f389ecd8b81d885
Amr Maket <amrmaket89@gmail.com>
2023-07-24 02:16:07 +0300

    added signup.php

--- php/sign_in.php
+++ php/sign_in.php
@@ -2,32 +2,33 @@
 
 include('connection.php');
 
-$username = $_POST['username'];
+$email = $_POST['email'];
 $password = $_POST['password'];
 
 
-$query = $mysqli->prepare('select user_id, username, password, first_name, last_name
+$query = $mysqli->prepare('select id, first_name, last_name ,email , password,
 from users
-where username=?');
+where email=?');
 
-$query->bind_param('s', $username);
+$query->bind_param('s', $email);
 $query->execute();
 
 $query->store_result();
-$query->bind_result($id, $username, $stored_password, $first_name, $last_name);
+$query->bind_result($id, $first_name, $last_name, $email, $password);
 $query->fetch();
 
 $num_rows = $query->num_rows();
 if ($num_rows == 0) {
     $response['status'] = "user not found";
 } else {
-    if ($password === $stored_password) {
+    if ($password === $password) {
         $response['status'] = 'Logged In';
-        $response['user_id'] = $id;
+        $response['id'] = 'id';
+        $response['email'] = $email;
         $response['first_name'] = $first_name;
-        $response['username'] = $username;
+        $response['last_name'] = $last_name;
     } else {
         $response['status'] = "Wrong Password";
     }
 }
-echo json_encode($response); -->
+echo json_encode($response);
--- script/script.js
+++ script/script.js
@@ -2,16 +2,30 @@ let login_btn = document.getElementById("login-btn")
 
 
 login_btn.addEventListener("click" , () =>{
-    let userName 
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
 })
 
+
+let signinData = new URLSearchParams();
+    signinData.append("email", email);
+    signinData.append("password", password);
+
 function getUser (){
-    fetch("http://localhost/Full-Stack-Mini-Project/php/sign_in.php")
-    .then((response) => {
-        return Response.json;
+    fetch("http://localhost/fullStack/php/sign_in.php", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: signinData
     })
-    .then((posts) => {
-        postsArray= posts;
+    .then((response) => response.json())
+    .then(data => {
+        if (data.status == "Logged In"){
+        localStorage.setItem("username", username)
+        window.location.href = "dashboard.html"
+        } else {
+            message.style.display = "block"
+        }
     })
-    .catch((error) => console.log(error))
 }
